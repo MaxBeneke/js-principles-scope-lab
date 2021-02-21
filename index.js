1123dea5407703c4ab712dfd987acb54b8f5bd88
Max Beneke <max.beneke@gmail.com>
2021-02-21 15:31:23 -0500

    commit tree fc9dddd4372d43140341861d7029604ab86c2c05

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    const bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "tim";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "kyle";
+}
+
